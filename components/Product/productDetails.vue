47a88a11b19753f29069af155db477034b805586
OsamaYouseff <oy6305616@gmail.com>
2024-12-07 17:46:12 +0200

    some design bugs

--- components/Product/productDetails.vue
+++ components/Product/productDetails.vue
@@ -4,24 +4,12 @@
     <div class="product-box lg:min-w-[870px] max-w-full grow">
       <!-- product -->
       <div
-        class="product-box mb-10 flex-center gap-4 cursor-pointer transition flex-wrap border-[#f6f7f8] border-b-2 min-w-full sm:min-w-full w-[370px] min-h-[630px] rounded py-5 text-center"
-      >
-        <div
-          class="img-container relative lg:grow-0 grow flex-col-center items-center"
-        >
-          <img
-            class="mb-4 h-[370px] w-[370px]"
-            :src="activeImage"
-            alt="macbook-img"
-          />
+        class="product-box mb-10 flex-center gap-4 transition flex-wrap border-[#f6f7f8] border-b-2 min-w-full sm:min-w-full w-[370px] min-h-[630px] rounded py-5 text-center">
+        <div class="img-container relative lg:grow-0 grow flex-col-center items-center">
+          <img class="mb-4 h-[370px] w-[370px]" :src="activeImage" alt="macbook-img" />
           <div class="sub-images flex-center">
-            <img
-              @click="activeImage = item"
-              v-for="item in subImages"
-              :class="activeImage == item ? 'active' : ''"
-              :src="`${item}`"
-              alt="product-img"
-            />
+            <img @click="activeImage = item" v-for="item in subImages" :class="activeImage == item ? 'active' : ''"
+              :src="`${item}`" alt="product-img" />
           </div>
         </div>
         <div class="content grow px-6">
@@ -32,10 +20,7 @@
           <div class="rating-wrapper flex gap-2 pb-2 border-b mb-4">
             <el-rate disabled allow-half v-model="ratingValue" size="small" />
             <span class="reviews text-[14px]">0 reviews</span>
-            <button
-              class="submit-review border-0 text-[14px]"
-              style="color: var(--primary)"
-            >
+            <button class="submit-review border-0 text-[14px]" style="color: var(--primary)">
               Submit a review
             </button>
           </div>
@@ -47,9 +32,7 @@
           </div>
 
           <!-- status -->
-          <div
-            class="product-status mb-4 flex-col-center gap-3 border-[#f6f7f8] border-b-2 pb-4"
-          >
+          <div class="product-status mb-4 flex-col-center gap-3 border-[#f6f7f8] border-b-2 pb-4">
             <div class="availability flex-between max-w-[250px]">
               <h3 class="font-medium">availability</h3>
               <span>In Stock</span>
@@ -70,15 +53,9 @@
             <div class="colors flex-between max-w-[273px] mb-4">
               <h3 class="font-medium">Select Color</h3>
               <div class="colors-wrapper flex-between gap-3">
-                <div
-                  class="parent flex-center cursor-pointer"
-                  v-for="color in Colors"
-                  :key="color"
-                  :style="{
-                    borderColor: activeColor == color ? color : 'transparent',
-                  }"
-                  @click="activeColor = color"
-                >
+                <div class="parent flex-center cursor-pointer" v-for="color in Colors" :key="color" :style="{
+                  borderColor: activeColor == color ? color : 'transparent',
+                }" @click="activeColor = color">
                   <span class="child" :style="{ background: color }"></span>
                 </div>
               </div>
@@ -87,12 +64,7 @@
             <!-- size -->
             <div class="sizes flex-between max-w-[244px]">
               <h3>Size</h3>
-              <el-menu
-                class="el-menu-demo"
-                mode="horizontal"
-                :ellipsis="false"
-                style="width: 116px"
-              >
+              <el-menu class="el-menu-demo" mode="horizontal" :ellipsis="false" style="width: 116px">
                 <el-sub-menu index="1">
                   <template #title>Size</template>
                   <el-menu-item index="1-1">XS</el-menu-item>
@@ -107,30 +79,21 @@
           </div>
 
           <!-- Amount & action btns -->
-          <div
-            class="amount-action flex-between py-4 border-[#f6f7f8] border-b-2 gap-3"
-          >
+          <div class="amount-action flex-between py-4 border-[#f6f7f8] border-b-2 gap-3">
             <div class="amount">
-              <el-input-number
-                v-model="amount"
-                :min="1"
-                :max="10"
-                @change="handleChange"
-                style="max-width: 116px; background: #f6f7f8"
-              />
+              <el-input-number v-model="amount" :min="1" :max="10" @change="handleChange"
+                style="max-width: 116px; background: #f6f7f8" />
             </div>
             <div class="action-btns flex gap-4">
               <button
                 class="add-to-cart border-0 text-[12px] max-h-[46px] flex gap-3 py-4 px-4 lg:px-6 rounded-md transition bg-[#ebf6ff] hover:bg-blue-100"
-                style="color: var(--primary)"
-              >
+                style="color: var(--primary)">
                 <img src="@/assets/icons/cart_2.svg" alt="cart-icon" />
                 <span>Add to cart</span>
               </button>
               <button
                 class="add-to-fav border-0 text-[14px] transition max-h-[46px] bg-[#ebf6ff] hover:bg-blue-100 p-4 rounded-md"
-                style="color: var(--primary)"
-              >
+                style="color: var(--primary)">
                 <img src="@/assets/icons/heart.svg" alt="heart-icon" />
               </button>
             </div>
@@ -139,32 +102,23 @@
           <!-- Social media -->
           <div class="social-media py-4 flex gap-4">
             <button
-              class="flex-center max-w-full bg-[#385c8e] gap-4 text-white py-4 px-6 rounded-md lg:w-[230px] h-[46px] text-sm lg:text-[16px]"
-            >
+              class="flex-center max-w-full bg-[#385c8e] gap-4 text-white py-4 px-6 rounded-md lg:w-[230px] h-[46px] text-sm lg:text-[16px]">
               <img src="@/assets/icons/facebook2.svg" alt="facebook-icon" />
               <span>Share on Facebook </span>
             </button>
             <button
-              class="flex-center max-w-full bg-[#06a8f4] gap-4 text-white py-4 px-6 rounded-md lg:w-[230px] h-[46px] text-sm lg:text-[16px]"
-            >
+              class="flex-center max-w-full bg-[#06a8f4] gap-4 text-white py-4 px-6 rounded-md lg:w-[230px] h-[46px] text-sm lg:text-[16px]">
               <img src="@/assets/icons/twitter2.svg" alt="twitter-icon" />
               <span>Share on Twitter </span>
             </button>
           </div>
         </div>
       </div>
+
       <!-- description -->
-      <div class="des">
-        <el-tabs
-          v-model="activeName"
-          class="demo-tabs bg-[#fafafb] rounded p-3"
-          @tab-click="handleClick"
-        >
-          <el-tab-pane
-            class="p-5"
-            label="Product Information"
-            name="product-info"
-          >
+      <div class="des min-h-[350px]">
+        <el-tabs v-model="activeName" class="demo-tabs bg-[#fafafb] rounded p-3" @tab-click="handleClick">
+          <el-tab-pane class="p-5" label="Product Information" name="product-info">
             <p class="text-[14px] mb-6 leading-7">
               Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, gravida
               et mattis vulputate, tristique ut lectus. Sed et lorem nunc.
@@ -199,7 +153,7 @@
       </div>
     </div>
 
-    <BestSeller />
+    <BestSeller class="xl:max-w-[269px]" />
   </section>
 </template>
 
@@ -228,7 +182,7 @@ import BestSeller from "./BestSeller.vue";
 const activeName = ref("product-info");
 
 const handleClick = (tab: TabsPaneContext, event: Event) => {
-  console.log(tab, event);
+  // console.log(tab, event);
 };
 
 watchEffect(() => {
@@ -251,6 +205,7 @@ watchEffect(() => {
   padding: 3px;
   border-radius: 4px;
 }
+
 .sub-images img.active {
   border: 2px solid var(--primary);
 }
@@ -259,6 +214,7 @@ watchEffect(() => {
   min-width: 150px;
   text-align: start;
 }
+
 .product-status span {
   min-width: 100px;
   text-align: start;
@@ -285,20 +241,20 @@ watchEffect(() => {
   --el-menu-horizontal-height: 38px;
 }
 
-.el-menu-demo > li {
+.el-menu-demo>li {
   border: 1px solid #eee;
   width: 116px;
   border-radius: 4px;
 }
 
-.demo-tabs > .el-tabs__content {
+.demo-tabs>.el-tabs__content {
   padding: 32px;
   color: #6b778c;
   font-size: 32px;
   font-weight: 600;
 }
 
-.el-tabs__nav > div {
+.el-tabs__nav>div {
   padding: 40px 20px;
   background: #000;
 }
--- pages/product/[id]/index.vue
+++ pages/product/[id]/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <section class="product-section container mx-auto py-10">
+  <section class="product-section container mx-auto pb-10">
     <NavigationPath />
     <ProductDetails />
+
     <!-- Related Products -->
     <div class="related-product-wrapper mt-10">
       <h2 class="text-center text-3xl font-medium uppercase mb-5">
         Related Products
       </h2>
       <div
-        class="related-products lg:ml-0 ml-2 flex-between md:justify-center md:flex-wrap lg:overflow-x-hidden overflow-x-scroll p-8 gap-3"
-      >
+        class="related-products lg:ml-0 ml-2 flex-between md:justify-center md:flex-wrap lg:overflow-x-hidden overflow-x-scroll p-8 gap-3">
         <Product v-for="product in 5" :key="product + 100" :product="product" />
       </div>
     </div>
