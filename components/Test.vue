544b95d170cfe3f97a778021d55204a4cc043181
OsamaYouseff <osamaseto2002@gmail.com>
2025-01-29 14:16:05 +0200

    last commit before starting converting code to a pug.js syntax.

--- components/Test.vue
+++ components/Test.vue
@@ -1,13 +1,34 @@
 <template lang="pug">
-  div
-    h1 Welcome to Nuxt 3 with Pug
+  
+  div.container.pt-10.text-red-500-mx-auto.text-center 
+    h1 Welcome to Nuxt 3 with Pug (just a training)  
     p Pug templates make HTML simpler.
+    
+    br
+    
     ul
-      li Item 1
-      li Item 2
-      li Item 3
-  </template>
+      li(v-for="item in items" :key="item.id" class="text-blue-500 cursor-pointer") {{ item }}
 
+    p(v-show="showMessage") this message will be shown on if the condition is true.
+
+    img(class="mx-auto mt-10 w-10 h-10" src="assets/icons/twitter.svg" alt="twitter-icon")
+    a(class="block text-[#03a9f4] cursor-pointer mb-5" href="https://twitter.com" target="_blank" ) Twitter
+    button(@click="handleClick") Click Me
+    
+    mixin card(title, description)
+      div.card
+        h2 {{ title }}
+        p {{ description }}
+
+
+    +card('This is a title of a mixin', 'This is a description of a mixin')
+
+</template>
 <script setup>
-// Add your logic here
+const items = ["link1", "link2", "link3"];
+const showMessage = true;
+
+const handleClick = () => {
+  alert("Hello there i am a button🫶");
+};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,6 @@
 
 <script setup lang="ts">
 import ContentSection from "~/components/Home/Content/ContentSection.vue";
-import Test from "~/components/Test.vue";
 
 useHead({ title: "Bouncer | Home " });
 </script>
